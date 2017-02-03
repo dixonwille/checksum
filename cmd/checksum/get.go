1cffcb9015e9694479c7d001cdbe53bb1ca9fbeb
Will Dixon <dixonwille@hotmail.com>
2017-02-03 17:21:21 -0500

    got the get working (check on why certain folders do not work

--- cmd/checksum/get.go
+++ cmd/checksum/get.go
@@ -1,1 +1,44 @@
 package main
+
+import (
+	"crypto"
+	"os"
+
+	"github.com/dixonwille/checksum"
+)
+
+func getChecksums(hash crypto.Hash, srcs []string) <-chan checksumResponse {
+	var channels []<-chan checksumResponse
+	for _, src := range srcs {
+		channels = append(channels, getChecksum(hash, src))
+	}
+	return channelHandler(channels)
+}
+
+func getChecksum(hash crypto.Hash, src string) <-chan checksumResponse {
+	csr := make(chan checksumResponse)
+	go func() {
+		defer close(csr)
+		var checksums []*checksum.FileChecksum
+		if info, err := os.Stat(src); err != nil {
+			csr <- newChecksumResponse(nil, err)
+			return
+		} else if info.IsDir() {
+			css, err := checksum.Directory(src, hash)
+			if err != nil {
+				csr <- newChecksumResponse(nil, err)
+				return
+			}
+			checksums = append(checksums, css...)
+		} else {
+			cs, err := checksum.File(src, hash)
+			if err != nil {
+				csr <- newChecksumResponse(nil, err)
+				return
+			}
+			checksums = append(checksums, cs)
+		}
+		csr <- newChecksumResponse(checksums, nil)
+	}()
+	return csr
+}
--- cmd/checksum/main.go
+++ cmd/checksum/main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"reflect"
 
 	"crypto"
 	_ "crypto/md5"
 	_ "crypto/sha1"
+	_ "crypto/sha512"
 
 	"strings"
 
+	"github.com/dixonwille/checksum"
 	cli "github.com/jawher/mow.cli"
 )
 
@@ -20,18 +24,19 @@ var (
 func init() {
 	supported["sha1"] = crypto.SHA1
 	supported["md5"] = crypto.MD5
+	supported["sha512"] = crypto.SHA512
 }
 
 func main() {
 	app := cli.App("checksum", "Get the checksum of a file, files, or directory")
 	app.Version("v version", "0.0.1")
 	app.Command("get", "Get the checksum of a file or files", func(cmd *cli.Cmd) {
-		cmd.Spec = "[-r] [-o] ALG SRC..."
-		alg := cmd.StringArg("ALG", "", "The algorithm to use for the checksum")
+		cmd.Spec = "[-r] [-o] HASH SRC..."
+		hash := cmd.StringArg("HASH", "", "The hash to use for the checksum")
 		recursive := cmd.BoolOpt("r recursive", false, "Get the checksum of folders and files recursively")
 		output := cmd.StringOpt("o output", "", "The output file of the checksum(s)")
 		srcs := cmd.StringsArg("SRC", nil, "File(s) or directory to get the checksum of")
-		cmd.Action = getCommand(output, alg, recursive, srcs)
+		cmd.Action = getCommand(output, hash, recursive, srcs)
 	})
 	app.Command("check", "Check the checksum of local files to a output file and return differing files", func(cmd *cli.Cmd) {
 		cmd.Spec = "[-r] CHECKSUMFILE [SRC...]"
@@ -47,14 +52,40 @@ func main() {
 	app.Run(os.Args)
 }
 
-func getCommand(output, alg *string, recursive *bool, srcs *[]string) func() {
+func getCommand(output, hash *string, recursive *bool, srcs *[]string) func() {
 	return func() {
 		err := validateSources(*recursive, *srcs)
 		if err != nil {
 			fmt.Println(err)
 			cli.Exit(1)
 		}
-		fmt.Printf("Checksum of %v in %s [output: %v] [recursively: %v]\n", *srcs, *alg, *output, *recursive)
+		hashMethod, ok := supported[*hash]
+		if !ok {
+			fmt.Printf("The hash provided is not supported: %s\n", *hash)
+			cli.Exit(1)
+		}
+		getChannel := getChecksums(hashMethod, *srcs)
+		checksums, ok := channelResponse(getChannel)
+		if !ok {
+			fmt.Println("Could not get the checksum of all the files")
+			cli.Exit(1)
+		}
+		var w *bufio.Writer
+		if *output == "" {
+			w = bufio.NewWriter(os.Stdout)
+		} else {
+			file, err := os.Create(*output)
+			if err != nil {
+				fmt.Println(err)
+				cli.Exit(1)
+			}
+			w = bufio.NewWriter(file)
+		}
+		err = iniFormatChecksums(w, *hash, checksums)
+		if err != nil {
+			fmt.Println(err)
+			cli.Exit(1)
+		}
 	}
 }
 
@@ -91,4 +122,53 @@ func validateSources(recursive bool, srcs []string) error {
 		}
 	}
 	return nil
+}
+
+func channelResponse(resChan <-chan checksumResponse) ([]*checksum.FileChecksum, bool) {
+	wasErr := false
+	var checksums []*checksum.FileChecksum
+	for response := range resChan {
+		if response.err != nil {
+			wasErr = true
+			fmt.Println(response.err)
+		}
+		if response.checksums != nil {
+			checksums = append(checksums, response.checksums...)
+		}
+	}
+	return checksums, !wasErr
+}
+
+func channelHandler(resChan []<-chan checksumResponse) <-chan checksumResponse {
+	mainChan := make(chan checksumResponse)
+	go func() {
+		defer close(mainChan)
+		cases := make([]reflect.SelectCase, len(resChan))
+		for i, ch := range resChan {
+			cases[i] = reflect.SelectCase{Dir: reflect.SelectRecv, Chan: reflect.ValueOf(ch)}
+		}
+
+		remaining := len(cases)
+		for remaining > 0 {
+			chosen, value, ok := reflect.Select(cases)
+			if !ok {
+				cases[chosen].Chan = reflect.ValueOf(nil)
+				remaining--
+				continue
+			}
+			var response checksumResponse
+			if value.CanInterface() {
+				var ok bool
+				resInterface := value.Interface()
+				response, ok = resInterface.(checksumResponse)
+				if !ok {
+					response = newChecksumResponse(nil, fmt.Errorf("Could not convert channel to checksum channel"))
+				}
+			} else {
+				response = newChecksumResponse(nil, fmt.Errorf("Channel value can not impliment an interface"))
+			}
+			mainChan <- response
+		}
+	}()
+	return mainChan
 }
